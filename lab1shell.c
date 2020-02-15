28e7fd0a8d5b8e33e091530fc1292e0608a76277
Zibathy <Zibathy@users.noreply.github.com>
2020-02-15 11:13:02 -0600

    type_prompt

--- lab1shell.c
+++ lab1shell.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+
 //#include "someHeader.h"
 
-void executableOnDisk(char someCommand);		    //execute command that is available on disk
-void builtIn(char anotherCommand);		          //execute command that has been coded into shell
-void errorInput();			                        //run if error detected
+void type_prompt();
+
+void executableOnDisk(char someCommand); 	//execute command that is available on disk
+void builtIn(char anotherCommand);		//execute command that has been coded into shell
+void errorInput();			    	//run if error detected
 
 
 int main(){
 	int continueMenu = 1; 			//function is for when true, continue to run program, else stop.
 	
+	char cmd[100], command[100], *parameters[20]; //environment variables
 
 	while(continueMenu == 1){
-		char selection = '';
+		type_prompt();
+		
+		read_command(command, parameters);
 
 
-		scanf("%f", &selection);
-		getchar();	//strips off newline character
+		scanf("%s", &selection);
+		getchar();			//strips off newline character
 		
 		if(selection == something){
 			executableOnDisk(something);
@@ -26,7 +37,7 @@ int main(){
 		else if(selection == something){
 			continueMenu = 0;      
 		 }
-		else{ 								//test for invalid input
+		else{ 				//test for invalid input
 			errorInput();
 		}
 	}
@@ -34,6 +45,18 @@ int main(){
 };
 
 
-void executableOnDisk(char someCommand);		    //execute command that is available on disk
-void builtIn(char anotherCommand);		          //execute command that has been coded into shell
-void errorInput();			                        //run if error detected
+void executableOnDisk(char someCommand); 	//execute command that is available on disk
+void builtIn(char anotherCommand);		//execute command that has been coded into shell
+void errorInput(){			        //run if error detected
+	fprintf("Invalid input.\n");
+}
+
+void type_prompt(){
+	static int firstTime = 1;
+	if(firstTime == 1){
+		const char* CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
+		write(STDOUT_FILENO, CLEAR_SCREEN_ANSI,12);
+		firstTime = 0;
+	}
+	printf("#");
+}
